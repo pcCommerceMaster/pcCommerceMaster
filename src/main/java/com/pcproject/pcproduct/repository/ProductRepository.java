47c066f2d6679b3dc731872166844df4fe6aa69b
Jeong Jihun <doksakim7@naver.com>
2026-02-25 15:30:20 +0900

    feat: feat: 주문 생성 재고 동시성 제어 적용 #55 - 충돌 해결 전

--- src/main/java/com/pcproject/pcproduct/repository/ProductRepository.java
+++ src/main/java/com/pcproject/pcproduct/repository/ProductRepository.java
@@ -17,4 +17,22 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select p from Product p where p.id = :id")
     Optional<Product> findByIdForUpdate(@Param("id") Long id);
+
+
+//    // =================================== 아래는 예은님 부분
+//    // 기본 단건 조회(soft delete 제외)
+//    Optional<Product> findByIdAndDeletedAtIsNull(Long id);
+//    // 재고 변경용 비관적 락 조회
+//    @Lock(LockModeType.PESSIMISTIC_WRITE)
+//    Optional<Product> findWithLockByIdAndDeletedAtIsNull(Long id);
+//
+//    // 상세 조회용
+//    @Query("""
+//            SELECT p
+//            FROM Product p
+//            JOIN FETCH p.admin
+//            WHERE p.id = :id
+//            AND p.deletedAt IS NULL
+//            """)
+//    Optional<Product> findWithAdminByIdAndDeletedAtIsNull(@Param("id") Long id);
 }
