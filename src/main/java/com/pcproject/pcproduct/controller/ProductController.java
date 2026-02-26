f514d866381bc71115de6b2bfca5ed852eaf922c
Jeong Jihun <doksakim7@naver.com>
2026-02-26 10:51:52 +0900

    refactor: 필요없는 것들 삭제

--- src/main/java/com/pcproject/pcproduct/controller/ProductController.java
+++ src/main/java/com/pcproject/pcproduct/controller/ProductController.java
@@ -21,43 +21,6 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
     private final ProductService productService;
 
-  // 검토필요 [20260225 03:25]===============================================================================
-//    @PostMapping
-//    public ResponseEntity<ApiResponse<ProductCreateResponse>> createProduct(
-//            @Valid @RequestBody ProductCreateRequest request,
-//            @RequestAttribute("adminId") Long adminId
-//            ) {
-//        ProductCreateResponse response = productService.createProduct(request, adminId);
-//
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(ApiResponse.created("상품 등록 완료", response));
-//    }
-// @PostMapping
-// public ApiResponse<ProductCreateResponse> createProduct(
-//         @Valid @RequestBody ProductCreateRequest request,
-//         HttpServletRequest httpRequest // 세션 직접 꺼내기 위해 추가
-// ) {
-//     // 인터셉터 구현 전이라 세션에서 직접 꺼내도록 임시 수정했습니다.
-//     HttpSession session = httpRequest.getSession(false);
-//     if (session == null || session.getAttribute(SessionConst.LOGIN_ADMIN) == null) {
-//         throw new CustomException(ErrorCode.UNAUTHORIZED);
-//     }
-//     LoginAdmin loginAdmin = (LoginAdmin) session.getAttribute(SessionConst.LOGIN_ADMIN);
-
-//     ProductCreateResponse response = productService.createProduct(request, loginAdmin.getId());
-//     return ApiResponse.created("상품 등록 완료", response);
-// }===================================================================================================
-    // 상품 등록
-//    @PostMapping
-//    public ResponseEntity<ApiResponse<ProductCreateResponse>> createProduct(
-//            @Valid @RequestBody ProductCreateRequest request,
-//            @RequestAttribute("adminId") Long adminId
-//            ) {
-//        ProductCreateResponse response = productService.createProduct(request, adminId);
-//
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(ApiResponse.created("상품 등록 완료", response));
-//    }
 
 // 세션에서 직접 꺼내도록 수정
     @PostMapping
